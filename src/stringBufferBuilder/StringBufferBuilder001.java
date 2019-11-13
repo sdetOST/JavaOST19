c506640f98f82c3ea5c3bf25ee3665ab0b26bfd0
“omerselim” <osturkakin@gmail.com>
2019-11-13 11:42:04 -0500

    commit_191113C

--- src/stringBufferBuilder/StringBufferBuilder001.java
+++ src/stringBufferBuilder/StringBufferBuilder001.java
@@ -8,39 +8,43 @@ public class StringBufferBuilder001 {
 		StringBuffer sb = new StringBuffer();
 		sb.append("1  123 56 ");
 		
-		System.out.println(sb);													// 1  123 56 
-		System.out.println(sb.length());											// 10
-		System.out.println(sb.capacity());										// 26
+		System.out.println(sb);												// 1  123 56 
+		System.out.println(sb.length());									// 10
+		System.out.println(sb.capacity());									// 26
 		
 		sb.trimToSize();															
-		System.out.println(sb.capacity());										// 10
-		System.out.println(sb.length());											// 10
-		System.out.println(sb);													// 1..123 56.
-		
-		System.out.println(sb.indexOf("5"));									// 7
-		System.out.println(sb.indexOf("56 "));									// 7
-		System.out.println(sb.indexOf("5555 "));								// -1
+		System.out.println(sb.capacity());									// 10
+		System.out.println(sb.length());									// 10
+		System.out.println(sb);												// 1..123 56.
+//INDEXOF		
+		System.out.println(sb.indexOf("5"));								// 7
+		System.out.println(sb.indexOf("56 "));								// 7
+		System.out.println(sb.indexOf("5555 "));							// -1
 		System.out.println(sb.lastIndexOf("12", 5))	;						// 3
 		System.out.println(sb.lastIndexOf("3 56"));							// 5
 		
+// REVERSE		
+		sb.reverse();					System.out.println(sb);				//	.65 321..1		
 		
-		sb.reverse();
-		System.out.println(sb);										//	.65 321..1		
+//DELETE		
+		sb.delete(2, 5);				System.out.println(sb);				// starts from index 2 but at the end its taking 5-1=4/(end-1)    
+																			//  INDEX  2 3 4   gone -> 621  1    
 		
-		sb.delete(2, 5); 											// starts from index 2 but at the end its taking 5-1=4/(end-1)    
-		System.out.println(sb);									//  INDEX  2 3 4   gone -> 621  1    
+		sb.delete(0, sb.length());		System.out.println(sb);				// make it EMTY
 		
-		sb.delete(0, sb.length());									// make it EMTY
-		System.out.println(sb);
+//ADD		
+		sb.append("abc");				System.out.println(sb); 			//abc					
 		
-		sb.append("abc"); 					
-		System.out.println(sb);
-		
+//INSERT		
 		StringBuffer s = new StringBuffer("dbca"); 
-		s.insert(2,"A");
-		s.replace(1, 4, "B");
-		System.out.println(s);
-
+		s.insert(2,"A");				System.out.println(s);				// dbAca
+//REPLACE		
+		s.replace(1, 4, "B");			System.out.println(s);				// dBa
+		
+//toString
+		
+		String str = s.toString();    	System.out.println(str);			// dBa
+		
 	}
 
 }
